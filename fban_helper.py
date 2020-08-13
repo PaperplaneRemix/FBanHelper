3a2b9841d3028d53ebe60eb05952217c22dbb186
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2020-08-13 19:28:36 +0530

    Send FBan's proof only once per fedchat.
    Prevents spam in the fedadmins chat.
    (Also improved logic, and formatted with `black`)

--- fban_helper.py
+++ fban_helper.py
@@ -12,25 +12,26 @@ plugin_category = "fedadmin"
 success_toasts = {609517172: ["New FedBan", "FedBan Reason update"]}
 
 
-@client.onMessage(command=("fban", plugin_category),
-                  outgoing=True,
-                  regex=r"fban(?: |$|\n)([\s\S]*)")
+@client.onMessage(
+    command=("fban", plugin_category), outgoing=True, regex=r"fban(?: |$|\n)([\s\S]*)"
+)
 async def fban(event: NewMessage.Event) -> None:
     """Ban a user in all your federations."""
     match = event.matches[0].group(1)
     args, kwargs = await client.parse_arguments(match)
-    reason = kwargs.get('reason', None)
+    reason = kwargs.get("reason", None)
     skipped = {}
     fbanned = []
+    failed = []
 
-    fban_admin_chats_list = client.config['userbot'].get(
-        'fban_admin_chats', None)
+    fban_admin_chats_list = client.config["userbot"].get("fban_admin_chats", None)
     if not fban_admin_chats_list:
         await event.answer(
-            "`Atleast one fedadmin chat should be set up for this to work!`")
+            "`Atleast one fedadmin chat should be set up for this to work!`"
+        )
         return
 
-    fban_admin_chats = set(int(x) for x in fban_admin_chats_list.split(','))
+    fban_admin_chats = set(int(x) for x in fban_admin_chats_list.split(","))
 
     if not args and event.reply_to_msg_id:
         reply = await event.get_reply_message()
@@ -40,27 +41,26 @@ async def fban(event: NewMessage.Event) -> None:
         return
 
     entity = await event.get_chat()
-    for user in args:
-        if isinstance(user, list):
-            continue
-        success = 0
-        failed = []
-        for chat in fban_admin_chats:
+    for chat in fban_admin_chats:
+        if event.reply_to_msg_id:
+            reply = await event.get_reply_message()
+            await reply.forward_to(chat)
+        for user in args:
+            if isinstance(user, list):
+                continue
             try:
-                async with client.conversation(chat, timeout=5,
-                                               exclusive=True) as conv:
+                async with client.conversation(chat) as conv:
                     spammer = await client.get_entity(user)
-                    if event.reply_to_msg_id:
-                        reply = await event.get_reply_message()
-                        await reply.forward_to(chat)
-                    await conv.send_message(f"/fban {spammer.id} {reason}")
+                    await conv.send_message(
+                        f'/fban {spammer.id}{" " + reason if reason else ""}'
+                    )
                     resp = await conv.get_response()
                     LOGGER.debug(f"FBan: {resp.text}")
                     await client.send_read_acknowledge(conv.chat_id, resp)
                     if resp.from_id in success_toasts and any(
-                            success_toast in resp.message
-                            for success_toast in success_toasts[resp.from_id]):
-                        success += 1
+                        success_toast in resp.message
+                        for success_toast in success_toasts[resp.from_id]
+                    ):
                         fbanned.append(user)
                         await asyncio.sleep(0.3)
                     else:
@@ -73,7 +73,7 @@ async def fban(event: NewMessage.Event) -> None:
                 skipped.update({str(user): failed})
     if fbanned:
         text = f"`Successfully FBanned:`\n"
-        text += ', '.join((f'`{user}`' for user in fbanned))
+        text += ", ".join((f"`{user}`" for user in fbanned))
         if reason:
             text += f"\n`Reason:` `{reason}`"
         e2 = await get_chat_link(entity, event.id)
@@ -81,6 +81,10 @@ async def fban(event: NewMessage.Event) -> None:
         await event.answer(text, log=("fban", log_msg))
     if skipped:
         text = "`Failed FBan requests:`\n"
-        text += '\n'.join((f"`{user}: [{', '.join(str(x) for x in feds)}]`"
-                           for user, feds in skipped.items()))
+        text += "\n".join(
+            (
+                f"`{user}: [{', '.join(str(x) for x in feds)}]`"
+                for user, feds in skipped.items()
+            )
+        )
         await event.answer(text, reply=True)
